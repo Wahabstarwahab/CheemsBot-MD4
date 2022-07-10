ec36b59fbd23702c39cf583bca584c101771ec04
Wahabstarwahab <108925586+Wahabstarwahab@users.noreply.github.com>
2022-07-10 20:13:55 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,19 +54,19 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['923499707942'] //ur owner number
-global.ownername = "🦄Dream Guy Xahab" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.vcardowner = ['923173069519'] //ur owner number
+global.ownername = "🦄 wahab" //ur owner name
+global.ytname = "YT: none" //ur yt chanel name
+global.socialm = "GitHub: none" //ur github or insta name
+global.location = "Pakistan, KPK , Abbottabad" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
-global.botname = 'Cheems Bot MD V4' //ur bot name
-global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
+global.owner = ['923173069519']
+global.ownertag = '923173069519' //ur tag number
+global.botname = 'wahab bot' //ur bot name
+global.linkz = "none" //your theme url which will be displayed on whatsapp
+global.websitex = "none" //ur website to be displayed
+global.botscript = 'none' //script link
 global.reactmoji = "😘" //ur menu react emoji
 global.themeemoji = "🦄" //ur theme emoji
 global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
